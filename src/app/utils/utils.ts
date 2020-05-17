d16d63d06a8e5240d0d56b6d576ec446726c2a11
Mike <mikesmallhelp@gmail.com>
2020-05-17 09:17:48 +0300

    Refactored utils

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,16 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+const testNumbers: string[] = ['0','1000', '9999999999999999'];
+
 export function doBreakTest(fixture: ComponentFixture<any>): void {   
     expect(isBreak(fixture)).toBe(false);
 }
 
 export function isBreak(fixture: ComponentFixture<any>): boolean {
-    const nativeElement = fixture.nativeElement;
     let isErrors = false;
 
     console.error = function () {
         isErrors = true;
     }
+
+    testInputs(fixture, testNumbers);
+    fixture.nativeElement.querySelectorAll('button').forEach(button => button.click());
+
+    return isErrors;
+}
+
+function testInputs(fixture: ComponentFixture<any>, inputValues: string[]): void {
+    const nativeElement = fixture.nativeElement;
 
     nativeElement.querySelectorAll('input').forEach(input =>
         ['0','1000', '9999999999999999'].forEach(inputValue => {
@@ -19,7 +29,4 @@ export function isBreak(fixture: ComponentFixture<any>): boolean {
             input.dispatchEvent(new Event('input'));
         })
     );
-    nativeElement.querySelectorAll('button').forEach(button => button.click());
-
-    return isErrors;
 }
